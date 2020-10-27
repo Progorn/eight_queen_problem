62489873f8f0ea0eb6d833f0c08d63721ed48df2
Progorn <16727624+Progorn@users.noreply.github.com>
2020-10-27 13:53:40 +0100

    Fixed board-print (one row was missing) Comments Revoved not used code.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,11 +1,10 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Game {
+    // Width and length of the board
     public static final int N = 8;
 
+    // We start with 1 to keep the zero as a no Queen indicator
     public static final int A = 1;
     public static final int B = 2;
     public static final int C = 3;
@@ -15,6 +14,7 @@ public class Game {
     public static final int G = 7;
     public static final int H = 8;
 
+    // We start with 0 because arrays are zero-indexed.
     public static final int ONE = 0;
     public static final int TWO = 1;
     public static final int THREE = 2;
@@ -24,10 +24,20 @@ public class Game {
     public static final int SEVEN = 6;
     public static final int EIGHT = 7;
 
+    /*
+     * We only need a one dimensional array:
+     * The key is the identifier of the row.
+     * The value show if there is a queen or not.
+     * 0 means no queen. Another number indicates the collum where the lines queen is placed.
+     */
     protected int[] board = new int[N];
 
+    // Counter to check if the alogorithm works.
     public int solutions = 0;
 
+    /**
+     * @param row int
+     */
     public void finishConfiguration(int row) {
         int placement;
 
@@ -44,44 +54,45 @@ public class Game {
         board[row] = 0;
     }
 
+    /**
+     * Method to look for queens that are able to attack the new queen.
+     * @param row int
+     * @return boolean
+     */
     private boolean queenIsSave(int row) {
         int collum = board[row];
+
+        // Check if there is already another queen in that row
         int index = indexOf(collum, board);
         if (index != row) {
             return false;
 
+        // We do not need to check our collum because the new queen overwrites the old one. (1-layer-array-structure)
+
         }else{
+            /*
+             * The following 2 parts are the same.
+             * Each one looks in a diagonal line from the queen and checks if it hits another one.
+             */
+
             int distance = 1;
+            // Bottom - Left
+            // Loop to check if the calculated position is still part of the game-board.
             while (row - distance >= ONE && collum - distance >= A)
             {
+                // Check if the queen at that row is in the position we would cross.
                 if (board[row - distance] == collum - distance)
                 {
                     return false;
                 }
                 distance++;
             }
+            // Reset distance
             distance = 1;
+            // Bottom - Right
             while (row - distance >= ONE && collum + distance <= N)
             {
                 if (board[row - distance] == collum + distance)
-                {
-                    return false;
-                }
-                distance++;
-            }
-            distance = 1;
-            while (row + distance <= N - 1 && collum - distance >= A)
-            {
-                if (board[row + distance] == collum - distance)
-                {
-                    return false;
-                }
-                distance++;
-            }
-            distance = 1;
-            while (row + distance <= N - 1 && collum + distance <= N)
-            {
-                if (board[row + distance] == collum + distance)
                 {
                     return false;
                 }
@@ -91,21 +102,41 @@ public class Game {
         return true;
     }
 
+    /**
+     * Prints the given board into std:out.
+     * 'X' show the position of a queen
+     */
     private void printBoard() {
         solutions++;
-        for (int counterX = A; counterX < N; counterX++) {
-            for (int counterY = A; counterY < N; counterY++) {
+        int row = N;
+        for (int counterX = ONE; counterX < N; counterX++) {
+            System.out.print(row + ":");
+            for (int counterY = ONE; counterY <= N; counterY++) {
                 if (board[counterX] == counterY) {
                     System.out.print('X');
                 } else {
                     System.out.print(' ');
                 }
             }
+            row--;
             System.out.println();
         }
-        System.out.println("--------");
+        System.out.print("  ");
+        char lineDescription = 'A';
+        for (int i = 0; i < N; i++) {
+            System.out.print(lineDescription);
+            lineDescription++;
+        }
+        System.out.println();
+        System.out.println("----------");
     }
 
+    /**
+     * Returns the first occurence of the int-number in the array of int-numbers.
+     * @param needle int
+     * @param haystack int[]
+     * @return int
+     */
     public static int indexOf(int needle, int[] haystack)
     {
         for (int i=0; i<haystack.length; i++)
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
 
+    /**
+     * main method to start the 8-queen-game-analysis.
+     * @param args String[]
+     */
     public static void main(String[] args) {
 	// write your code here
         Game game = new Game();
